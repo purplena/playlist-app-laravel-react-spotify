78a81c6fc294c9a344576e8c67c065bbcf1f3f6e
Purplena <lkhramova14@gmail.com>
2023-12-02 11:25:47 +0100

    feat: api root discovery

--- routes/web.php
+++ routes/web.php
@@ -17,5 +17,3 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', fn () => view('home'));
-Route::get('/companies', [CompanyController::class, 'index']);
-Route::get('/companies/{company}', [CompanyController::class, 'show']);
