1cd997a876777d62914c4fd124ea314d9d1af332
Purplena <lkhramova14@gmail.com>
2023-12-28 12:44:38 +0100

    feat: check if a playlist have been deleted on Spotify

--- app/Console/Commands/SyncPlaylist.php
+++ app/Console/Commands/SyncPlaylist.php
@@ -2,10 +2,20 @@
 
 namespace App\Console\Commands;
 
+use App\Models\Company;
+use App\Services\SpotifyApi;
 use Illuminate\Console\Command;
+use Illuminate\Support\Arr;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class SyncPlaylist extends Command
 {
+
+  public function __construct(protected SpotifyApi $spotifyApi)
+  {
+    parent::__construct();
+  }
   /**
    * The name and signature of the console command.
    *
@@ -25,18 +35,41 @@ class SyncPlaylist extends Command
    */
   public function handle()
   {
-    dd("ok");
-    // $api = $this->spotifyApi->getCompanyApiInstance($company);
-    // $userId = "89l0ygza24p05l6nz4n1bj1ut";
+    foreach (Company::all() as $company) {
+      try {
+        $api = $this->spotifyApi->getCompanyApiInstance($company);
+        try {
+          $userId = Arr::get($company->spotify_playlist_data, 'user_id');
+          $playlists = $api->getUserPlaylists($userId, [
+            'limit' => 10
+          ]);
+          if (!collect($playlists->items)->contains('id', Arr::get($company->spotify_playlist_data, 'playlist.id'))) {
+            $company->update([
+              'spotify_playlist_data' => null
+            ]);
+          } else {
+            $this->addTracksToPlaylist($company);
+          }
+        } catch (Throwable $t) {
+          //problem with user_id in spotify_playlist_data
+          // spotify_playlist_data is corrupted
+          continue;
+        }
+      } catch (Throwable $t) {
+        // problem with refresh token
+        // spotify_playlist_data is corrupted
+        // make an action here
+        //for example send an email that a "reconnection to spotify is needed"
+        Log::error('Problem with refresh token', ['company' => $company]);
+        $company->update([
+          'spotify_playlist_data' => null
+        ]);
+        continue;
+      }
+    }
+  }
 
-    // $playlists = $api->getUserPlaylists($userId, [
-    //   'limit' => 5
-    // ]);
-
-    // dd($playlists);
-    // dd(Arr::get($company->spotify_playlist_data, 'playlist.id'));
-    // dd($api->getPlaylist(Arr::get($company->spotify_playlist_data, 'playlist.id')));
-    // dd($api->getPlaylist("2BFhGYOAsjkitd34cD2a00"));
-    // dd($api->getPlaylist("1S7g8GqdHEzrsZjdd4oAaN"));
+  protected function addTracksToPlaylist(Company $company): void
+  {
   }
 }
