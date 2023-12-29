10d7cd86b434c1bc45ac5ae828e6ff755369b076
Purplena <lkhramova14@gmail.com>
2023-12-29 17:06:57 +0100

    feat: added eslint plugin to vite

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from "vite";
-import laravel from "laravel-vite-plugin";
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import eslint from 'vite-plugin-eslint';
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/js/index.js"],
-            refresh: true,
-        }),
-    ],
+  plugins: [
+    laravel({
+      input: ['resources/js/index.js'],
+      refresh: true,
+    }),
+    eslint(),
+  ],
 });
