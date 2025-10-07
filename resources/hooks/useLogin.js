8a3cdf4a2605a2c26b24fc626f002c3c1947ac32
purplena <lkhramova14@gmail.com>
2025-10-07 16:32:46 +0200

    install laravel telescope

--- resources/hooks/useLogin.js
+++ resources/hooks/useLogin.js
@@ -8,8 +8,6 @@ export const useLogin = () => {
   const [errors, setErrors] = useState(null);
 
   const login = (email, password) => {
-    return new Promise((resolve, reject) => {
-      axios.get('/sanctum/csrf-cookie').then(() => {
         return axios
           .post(`${apiUrl}/user/login`, {
             email,
@@ -19,7 +17,8 @@ export const useLogin = () => {
             if (response.data.user) {
               setUser(response.data.user);
             }
-            resolve(response);
+
+            return response;
           })
           .catch((error) => {
             if (!error.response?.data?.status) {
@@ -30,46 +29,14 @@ export const useLogin = () => {
             if (error.response.data.email || error.response.data.password) {
               setErrors(error.response.data);
             }
-            reject(error);
+
+            return error.response; 
           });
-      });
-    });
   };
-  // Which approach you prefer?
-  // const login = async (email, password) => {
-  //     try {
-  //         // make request first to sanctum/csrf-cookie endpoint
-  //         // to initialize CSRF protection for the application
-  //         await axios.get("/sanctum/csrf-cookie");
 
-  //         const payload = {
-  //             email,
-  //             password,
-  //         };
-
-  //         const response = await axios.post(`${apiUrl}/user/login`, payload, {
-  //             headers: { Accept: "application/json" },
-  //         });
-
-  //         if (response.data.user) {
-  //             setUser(response.data.user);
-  //         }
-  //         return response;
-  //     } catch (error) {
-  //         if (!error.response?.data?.status) {
-  //             setErrors({
-  //                 loginError: [error.response.data.message],
-  //             });
-  //         }
-  //         if (error.response.data.email || error.response.data.password) {
-  //             setErrors(error.response.data);
-  //         }
-  //         throw error;
-  //     }
-  // };
-
-  return {
-    login,
-    errors,
+    return {
+      login,
+      errors,
   };
 };
+
